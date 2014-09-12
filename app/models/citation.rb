2c407be1836f5bacf40816a8b0446d4b9de330b4
Erik Hetzner <ehetzner@plos.org>
2014-09-12 15:28:09 -0700

    citations should be unique for a->b

--- app/models/citation.rb
+++ app/models/citation.rb
@@ -1,4 +1,5 @@
 class Citation < ActiveRecord::Base
   belongs_to :cited_paper, class: Paper
   belongs_to :citing_paper, class: Paper
+  validates :citing_paper, uniqueness: {scope: :cited_paper}
 end
--- test/models/citation_test.rb
+++ test/models/citation_test.rb
@@ -7,4 +7,13 @@ class CitationTest < ActiveSupport::TestCase
     citation = Citation.new(text: 'xyz', cited_paper: a, cited_paper: b)
     assert citation.save
   end
+
+  test 'Citations are unique for a paper combination' do
+    a = Paper.new(uri: 'http://example.org/a')
+    b = Paper.new(uri: 'http://example.org/b')
+    assert Citation.new(text: 'foo', cited_paper: a, cited_paper: b).save
+    assert Citation.new(text: 'bar', cited_paper: b, cited_paper: a).save
+    assert_not Citation.new(text: 'baz', cited_paper: a, cited_paper: b).save
+    assert_not Citation.new(text: 'bay', cited_paper: b, cited_paper: a).save
+  end
 end
