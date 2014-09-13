592ffc7d2c5caac90e8a39f081bde16322d3bef5
Marc Rohloff <marc@marcrohloff.com>
2014-09-12 20:04:41 -0600

    Fix some previous specs

--- test/models/citation_test.rb
+++ test/models/citation_test.rb
@@ -5,17 +5,17 @@ class CitationTest < ActiveSupport::TestCase
   test 'can create Citation' do
     a = Paper.new(uri: 'http://example.org/a')
     b = Paper.new(uri: 'http://example.org/b')
-    citation = Citation.new(text: 'xyz', cited_paper: a, cited_paper: b)
+    citation = Citation.new(text: 'xyz', citing_paper: a, cited_paper: b)
     assert citation.save
   end
 
   test 'Citations are unique for a paper combination' do
     a = Paper.new(uri: 'http://example.org/a')
     b = Paper.new(uri: 'http://example.org/b')
-    assert Citation.new(text: 'foo', cited_paper: a, cited_paper: b).save
-    assert Citation.new(text: 'bar', cited_paper: b, cited_paper: a).save
-    assert_not Citation.new(text: 'baz', cited_paper: a, cited_paper: b).save
-    assert_not Citation.new(text: 'bay', cited_paper: b, cited_paper: a).save
+    assert Citation.new(text: 'foo', citing_paper: a, cited_paper: b).save
+    assert Citation.new(text: 'bar', citing_paper: b, cited_paper: a).save
+    assert_not Citation.new(text: 'baz', citing_paper: a, cited_paper: b).save
+    assert_not Citation.new(text: 'bay', citing_paper: b, cited_paper: a).save
   end
 
   test 'should round trip text json' do
@@ -34,7 +34,7 @@ class CitationTest < ActiveSupport::TestCase
   test 'can set text to nil' do
     p = Paper.new(uri: 'http://example.org/a')
 
-    a = Citation.create(cited_paper:p, text: nil )
+    a = Citation.create(citing_paper:p, text: nil )
     assert_nil(a.text)
 
     b = Citation.find(a.id)
