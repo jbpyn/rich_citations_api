de57b90e7de9504372af23e8a6782c4673fcf46b
Erik Hetzner <ehetzner@plos.org>
2014-09-17 14:03:47 -0700

    fix test for citation_group creation

--- test/models/citation_group_test.rb
+++ test/models/citation_group_test.rb
@@ -2,14 +2,10 @@ require 'test_helper'
 
 class CitationGroupTest < ActiveSupport::TestCase
   test 'can create a citation group' do
-    a = papers(:a)
-    b = papers(:b)
-    c = papers(:c)
+    r1 = Reference.new(text: 'foo', citing_paper: papers(:d), cited_paper: papers(:e), ref:'ref-1', index: 1, uri:'uri://1')
+    r2 = Reference.new(text: 'bar', citing_paper: papers(:d), cited_paper: papers(:f), ref:'ref-1', index: 2, uri:'uri://2')
 
-    r1 = Reference.new(text: 'foo', citing_paper: a, cited_paper: b, ref:'ref-1', index: 1, uri:'uri://1')
-    r2 = Reference.new(text: 'bar', citing_paper: a, cited_paper: c, ref:'ref-1', index: 2, uri:'uri://2')
-
-    g = CitationGroup.new(citing_paper: a,
+    g = CitationGroup.new(citing_paper: papers(:d),
                           section: 'Foo',
                           text: '[1,2]',
                           ellipses_before: true,
@@ -19,10 +15,6 @@ class CitationGroupTest < ActiveSupport::TestCase
                           references: [r1, r2])
     assert(g.save)
     assert_equal(g.references, [r1, r2])
-    assert_equal(g.references[0].cited_paper, b)
-  end
-
-  test '' do
-    
+    assert_equal(g.references[0].cited_paper, papers(:e))
   end
 end
