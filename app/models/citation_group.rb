a8b76ff37a47f6d8812ac97ac35b13663b43ad05
Erik Hetzner <ehetzner@plos.org>
2014-09-18 13:45:54 -0700

    cascade delete citation_groups to citation_group_reference

--- app/models/citation_group.rb
+++ app/models/citation_group.rb
@@ -1,5 +1,6 @@
 class CitationGroup < ActiveRecord::Base
   belongs_to :citing_paper, foreign_key: :citing_paper_id, class: Paper
-  has_many :citation_group_references, -> { order('position ASC') }
+  has_many :citation_group_references, -> { order('position ASC') },
+           dependent: :destroy
   has_many :references, through: :citation_group_references, class: Reference
 end
--- test/models/citation_group_test.rb
+++ test/models/citation_group_test.rb
@@ -35,4 +35,15 @@ class CitationGroupTest < ActiveSupport::TestCase
     assert(g.save)
     assert_equal([references(:ref_2), references(:ref_1)], g.references)
   end
+
+  test 'citation_group destroys citation_group_references, but not references ' do
+    r1 = Reference.new(text: 'foo', citing_paper: papers(:d), cited_paper: papers(:e), ref:'ref-1', index: 1, uri:'uri://1')
+    g = CitationGroup.new(citing_paper: papers(:d),
+                          references: [r1])
+    g.save
+    cgr = g.citation_group_references[0]
+    assert(g.destroy)
+    assert(cgr.destroyed?)
+    assert_not(r1.destroyed?)
+  end
 end
