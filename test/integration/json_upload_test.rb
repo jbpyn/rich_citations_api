a11eea8b860983410e9a3160eae6759bb9495ad9
Erik Hetzner <ehetzner@plos.org>
2014-10-06 11:12:11 -0700

    add test for real article (not demo)

--- test/integration/json_upload_test.rb
+++ test/integration/json_upload_test.rb
@@ -1,5 +1,5 @@
 require 'test_helper'
- 
+
 class JsonUploadTest < ActionDispatch::IntegrationTest
   test 'complete JSON upload works' do
     json_file = File.join(Rails.root, 'test', 'fixtures', 'journal.pone.0000000.json')
@@ -9,6 +9,17 @@ class JsonUploadTest < ActionDispatch::IntegrationTest
          'Content-Type' => Mime::JSON.to_s)
     assert_response(:created, @response.body)
     assert_equal('http://www.example.com/papers?uri=http%3A%2F%2Fdx.doi.org%2F10.1371%2Fjournal.pone.0000000',
+                 @response.headers['Location'])
+  end
+
+  test 'complete JSON upload works (2)' do
+    json_file = File.join(Rails.root, 'test', 'fixtures', 'journal.pone.0107541.json')
+    post('/papers?api_key=841c5d42-2ca3-42fc-8eda-87fbccc1f4ca',
+         File.read(json_file).to_s,
+         'Accept'       => Mime::JSON.to_s,
+         'Content-Type' => Mime::JSON.to_s)
+    assert_response(:created, @response.body)
+    assert_equal('http://www.example.com/papers?uri=http%3A%2F%2Fdx.doi.org%2F10.1371%2Fjournal.pone.0107541',
                  @response.headers['Location'])
   end
 end
