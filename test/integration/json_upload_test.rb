0facb576567e9368b1eaa6aae9d2f4f3e4b3584d
Erik Hetzner <ehetzner@plos.org>
2014-10-15 13:49:04 -0700

    fix json upload test

--- test/integration/json_upload_test.rb
+++ test/integration/json_upload_test.rb
@@ -4,14 +4,14 @@ class JsonUploadTest < ActionDispatch::IntegrationTest
   SANITIZER = Rails::Html::PermitScrubber.new
   SANITIZER.tags = %w[a em i strong b u cite q mark abbr sub sup s wbr]
 
-  def deep_compare(new, original, path="")
+  def deep_compare(doi_uri, new, original, path="")
     if new.is_a?(Array)
       new.each_index do |i|
-        deep_compare(new[i], original[i], path + "[#{i}]")
+        deep_compare(doi_uri, new[i], original[i], "#{path}[#{i}]")
       end
     elsif new.is_a?(Hash)
       new.each_key do |key|
-        deep_compare(new[key], original[key], path + "/#{key}")
+        deep_compare(doi_uri, new[key], original[key], "#{path}/#{key}")
       end
     elsif new.is_a?(String) && (new.match(/T00:00:00.000Z$/))
       # ignore these time diffs
@@ -19,30 +19,30 @@ class JsonUploadTest < ActionDispatch::IntegrationTest
     elsif original.nil?
       # ignore when we have generated a URI
       unless new.match(/^cited:/)
-        assert_equal(original, new, path)
+        assert_equal(original, new, "#{doi_uri}: #{path}")
       end
     elsif new.is_a?(String)
       # check scrubbed version too
       scrubbed = Loofah.fragment(original).scrub!(SANITIZER).scrub!(:strip).scrub!(:nofollow).to_s
       if ((original != new ) && (scrubbed != new))
-        assert_equal(original, new, path)
+        assert_equal(original, new, "#{doi_uri}: #{path}")
       end
     else
-      assert_equal(original, new, path)
+      assert_equal(original, new, "#{doi_uri}: #{path}")
     end
   end
         
   test 'complete JSON upload works' do
-    ['journal.pone.0000000', 'journal.pmed.1000317'].each do |i|
-      json_file = File.join(Rails.root, 'test', 'fixtures', "#{i}.json")
+    Dir.glob(File.join(Rails.root, 'test', 'fixtures', '*.json')).each do |json_file|
+      doi_uri = 'http://dx.doi.org/10.1371/' + json_file.match(/(journal.*).json$/)[1]
       post('/papers?api_key=841c5d42-2ca3-42fc-8eda-87fbccc1f4ca',
            File.read(json_file).to_s,
            'Accept'       => Mime::JSON.to_s,
            'Content-Type' => Mime::JSON.to_s)
       assert_response(:created, @response.body)
-      assert_equal("http://www.example.com/papers?uri=http%3A%2F%2Fdx.doi.org%2F10.1371%2F#{i}",
+      assert_equal("http://www.example.com/papers?uri=#{URI.encode_www_form_component(doi_uri)}",
                    @response.headers['Location'])
-      get('/papers', {uri: "http%3A%2F%2Fdx.doi.org%2F10.1371%2F#{i}", include: 'cited'},
+      get('/papers', {uri: doi_uri, include: 'cited'},
           'Accept' => Mime::JSON.to_s)
       original = MultiJson.load(File.read(json_file).to_s)
       # API will add truncated_before, truncated_after if they are empty
@@ -52,7 +52,7 @@ class JsonUploadTest < ActionDispatch::IntegrationTest
         context['truncated_after'] = false unless context.key?('truncated_after')
       end
       new = MultiJson.load(@response.body.to_s)
-      deep_compare(new, original)
+      deep_compare(doi_uri, new, original)
     end
   end
 end
