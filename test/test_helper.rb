c94a49ad9dfc16eb57370c3f9706e57940dde39a
Erik Hetzner <ehetzner@plos.org>
2014-10-23 13:39:44 -0700

    enable transaction fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,7 @@ require 'mocha/test_unit'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  self.use_transactional_fixtures = true
 
   # Add more helper methods to be used by all tests here...
 
