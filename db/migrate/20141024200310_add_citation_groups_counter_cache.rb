fa3daeebe1ca3a9db64998efd2c0cb04624fb1d3
Erik Hetzner <ehetzner@plos.org>
2014-10-24 15:55:25 -0700

    fix db migration for faster execution

--- db/migrate/20141024200310_add_citation_groups_counter_cache.rb
+++ db/migrate/20141024200310_add_citation_groups_counter_cache.rb
@@ -3,7 +3,7 @@ class AddCitationGroupsCounterCache < ActiveRecord::Migration
     add_column :references, :mention_count, :integer, default: 0
 
     Reference.reset_column_information
-    Reference.all.each do |r|
+    Reference.find_each do |r|
       r.update_attribute :mention_count, r.citation_group_references.count
     end
   end
