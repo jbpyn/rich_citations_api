87f01c773ef8763350e945e37e06214fef7be6e7
Erik Hetzner <ehetzner@plos.org>
2014-10-24 10:17:37 -0700

    refactor citegrap csv database backend specific code

--- app/controllers/v0/papers_controller.rb
+++ app/controllers/v0/papers_controller.rb
@@ -88,15 +88,13 @@ module V0
               q = Reference
                   .joins('LEFT OUTER JOIN "papers" "cited_papers"  ON "cited_papers"."id"  = "references"."cited_paper_id"')
                   .joins('LEFT OUTER JOIN "papers" "citing_papers" ON "citing_papers"."id" = "references"."citing_paper_id"')
+                  .select('citing_papers.uri as citing', 'cited_papers.uri as cited')
+              f = -> (d) { streamer.write_line(d['citing'], d['cited']) }
               if (ActiveRecord::Base.connection.adapter_name == 'PostgreSQL')
                 # use postgres_cursor
-                q.select('citing_papers.uri as citing', 'cited_papers.uri as cited').each_row do |d|
-                  streamer.write_line(d['citing'], d['cited'])
-                end
+                q.each_row(&f)
               else
-                q.pluck('citing_papers.uri', 'cited_papers.uri').each do |d|
-                  streamer.write_line(d[0], d[1])
-                end
+                q.each(&f)
               end
             ensure
               streamer.close
